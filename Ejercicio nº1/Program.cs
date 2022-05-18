e9dd3182af647d06ab49f02d4e2895ec007a7bf9
Khause <Khause132@gmail.com>
2022-05-18 20:21:35 -0300

    Add project files.

---
+++ Ejercicio nº1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.WriteLine("Buenas, por favor ingrese su nombre");
+Console.WriteLine();
+string nombre = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("¡Hola " + nombre + "!");
+Console.WriteLine();
+Console.WriteLine("Esto es todo lo que hace este programa, si desea ingresar" +
+    " otro nombre o simplemente quiere repetirlo, ingrese S, si desea cerrar el programa, ingrese N");
+Console.WriteLine();
+string Finalizar = Console.ReadLine();
+while (Finalizar.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Buenas, por favor ingrese su nombre");
+    Console.WriteLine();
+    nombre = Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine("¡Hola " + nombre + "!");
+    Console.WriteLine();
+    Console.WriteLine("Esto es todo lo que hace este programa, si desea ingresar" +
+        " otro nombre o simplemente quiere repetirlo, ingrese S, si desea cerrar el programa, ingrese N");
+    Console.WriteLine();
+    Finalizar = Console.ReadLine();
+}
+
+if (Finalizar.ToUpper() == "N")
+{
+    Console.WriteLine();
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("Opción no válida");
+}
+
